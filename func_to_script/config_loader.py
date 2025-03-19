a61c9b5621824f2e00b139f30c50fb784f4db810
Chris Hughes <31883449+Chris-hughes10@users.noreply.github.com>
2025-03-19 12:03:34 +0000

    Update Struct
    Update struct to have more dict like behaviours

--- func_to_script/config_loader.py
+++ func_to_script/config_loader.py
@@ -10,6 +10,25 @@ class Struct:
 
     def __repr__(self):
         return str(self.__dict__)
+        
+    # Make it behave like a dictionary
+    def __iter__(self):
+        return iter(self.__dict__)
+    
+    def __getitem__(self, key):
+        return self.__dict__[key]
+    
+    def items(self):
+        return self.__dict__.items()
+    
+    def keys(self):
+        return self.__dict__.keys()
+    
+    def values(self):
+        return self.__dict__.values()
+    
+    def get(self, key, default=None):
+        return self.__dict__.get(key, default)
 
 
 def load_config_from_yaml(config_path):
